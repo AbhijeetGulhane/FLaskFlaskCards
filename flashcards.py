4dcdac8f122961e193fc480fc71d28e76914c3ab
Abhijeet Gulhane <abhijeet.gulhane.317@gmail.com>
2023-05-03 19:23:43 -0400

    Homepage and base web app complete

--- flashcards.py
+++ flashcards.py
@@ -1,8 +1,21 @@
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
-
+count = 0
 
 @app.route("/")
 def welcome():
     return "Welcome to the flash cards application!!!"
+
+
+@app.route("/viewcount")
+def viewcount():
+    global count
+    count += 1
+    return "Page was viewed " + str(count) + " times."
+
+
+@app.route("/date")
+def date():
+    return "This page was serverd at " + str(datetime.now())
